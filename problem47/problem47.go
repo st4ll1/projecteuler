d694d9c10bbd9dd6a451cdd5d6955dbd6af427e0
Andreas Stallinger <astallinger.mmt-m2013@fh-salzburg.ac.at>
2015-10-09 12:11:59 +0200

    problem47 solved

--- problem47/problem47.go
+++ problem47/problem47.go
@@ -2,12 +2,45 @@ package main
 
 import (
     "fmt"
+	"github.com/kavehmz/prime"
 )
 
-func isDistincPrime(number int){
-
-}
-
 func main(){
-    fmt.Println("Solution Problem 47: ",4)
+	var digits int = 4 
+	var max uint64 = 10
+		
+	for i := 1; i < 8; i++ {		
+		max *= 10
+	}
+	
+	primes := prime.Primes(max);
+	
+	var dpf int = 0;
+	for i := 1; uint64(i) < max; i++ {
+		var factors int = 0
+		var currentNumber uint64 = uint64(i)
+		for p := 0 ; p < len(primes); p++ {
+			if currentNumber%primes[p] != 0 {
+				continue
+			}
+			factors++
+			for currentNumber%primes[p] == 0 {				
+				currentNumber /= primes[p]				
+			}			
+			if currentNumber == 1 {
+				if factors == digits{
+					dpf++
+				} else {
+					dpf = 0
+				}
+				break
+			}		
+		}
+		if dpf == digits{
+    		fmt.Println("Solution Problem 47: ",i-digits+1)	
+			break	
+		}
+	}	
+	
+	
 }
